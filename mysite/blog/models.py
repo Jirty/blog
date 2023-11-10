7f799f8d9fd5d71616a9412ec8286b2e1ca54c2f
ар <riven579@gmail.com>
2023-11-10 19:53:45 +0200

    first commit after migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)  # перевизначення стандартного методу get_queryset
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -19,6 +25,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
