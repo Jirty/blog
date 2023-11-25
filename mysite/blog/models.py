bdc62cf649829efca1dffcc8a0ae79ab351d1ea8
ар <riven579@gmail.com>
2023-11-25 16:58:28 +0200

    urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,20 +5,21 @@ from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
+    # перевизначення стандартного методу get_queryset для того щоб діставати лише опубліковані пости
     def get_queryset(self):
         return super().get_queryset()\
-                      .filter(status=Post.Status.PUBLISHED)  # перевизначення стандартного методу get_queryset
+                      .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
-
+    # Основні поля поста
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
+                            unique_for_date='publish', primary_key=True)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -27,6 +28,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    # Менеджери об'єктів для управління постами
     objects = models.Manager()
     published = PublishedManager()
 
@@ -36,9 +39,11 @@ class Post(models.Model):
             models.Index(fields=['-publish'])
                    ]
 
+    # Представлення об'єкта як рядка
     def __str__(self):
         return self.title
 
+    # Отримання абсолютного URL для поста на основі дати і slug
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[self.publish.year,
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
+
+
+
+
+#     def post_list(request):
+#         post_list = Post.published.all()
+#         paginator = Paginator(post_list, 3)
+#         page_number = request.GET.get("page", 1)
+#         try:
+#             posts = paginator.page(page_number)
+#         except EmptyPage:
+#             posts = paginator.page(paginator.num_pages)
+#         except PageNotAnInteger:
+#             posts = paginator.page(1)
+#         return render(request, "blog/post/list.html", {"posts": posts})
+#
+# def post_detail(request, year, month, day, post):
+#     post = get_object_or_404(
+#             Post,
+#             status=Post.Status.PUBLISHED,
+#             slug=post,
+#             publish__year=year,
+#             publish__month=month,
+#             publish__day=day,
+#         )
+#     return render(request, "blog/post/detail.html", {"post": post})
